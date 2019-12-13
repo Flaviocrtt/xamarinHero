b34a691485c6bef6cb4db2628194227a6ed6fdc0
FlavioCrtt <flavio777crtt@hotmail.com>
2019-12-13 01:19:14 -0300

    Utilizando SharedResources no xaml

--- Source/Phoneword/Phoneword/Phoneword/MainPage.xaml.cs
+++ Source/Phoneword/Phoneword/Phoneword/MainPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -12,9 +12,57 @@ namespace Phoneword
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class MainPage : ContentPage
     {
+        public string _translatedNumber { get; private set; }
+
         public MainPage()
         {
             InitializeComponent();
         }
+
+        private void OnTranslate(object sender, EventArgs e)
+        {
+            string enteredNumber = phoneNumberText.Text;
+            _translatedNumber = Core.PhonewordTranslator.ToNumber(enteredNumber);
+
+            if (!string.IsNullOrEmpty(_translatedNumber))
+            {
+                callButton.IsEnabled = true;
+                callButton.Text = "Call " + _translatedNumber;
+            }
+            else
+            {
+                callButton.IsEnabled = false;
+                callButton.Text = "Call";
+            }
+        }
+
+        async void OnCall(object sender, System.EventArgs e)
+        {
+            if (await this.DisplayAlert(
+                "Dial a Number",
+                "Would you like to call " + _translatedNumber + "?",
+                "Yes",
+                "No"))
+            {
+                try
+                {
+                    PhoneDialer.Open(_translatedNumber);
+                }
+                catch (ArgumentNullException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone number was not valid.", "OK");
+                }
+                catch (FeatureNotSupportedException)
+                {
+                    await DisplayAlert("Unable to dial", "Phone dialing not supported.", "OK");
+                }
+                catch (Exception)
+                {
+                    // Other error has occurred.
+                    await DisplayAlert("Unable to dial", "Phone dialing failed.", "OK");
+                }
+            }
+        }
+
     }
 }
