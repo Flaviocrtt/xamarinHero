62be04881d83c6549e7493b3aa98a71bcc8090ab
FlavioCrtt <flavio777crtt@hotmail.com>
2019-12-26 01:12:10 -0300

    Implementação de masterdetailpage com páginas distintas (navegação em gaveta)

--- Source/masterdetail/Astronomy/AstronomyMasterPage.xaml.cs
+++ Source/masterdetail/Astronomy/AstronomyMasterPage.xaml.cs
@@ -1,20 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿using Astronomy.Pages;
 using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
+using System;
 
 namespace Astronomy
 {
-    [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class AstronomyMasterPage : MasterDetailPage
+    public partial class AstronomyMasterPage : ContentPage
     {
+        public event EventHandler<PageType> PageSelected;
+
         public AstronomyMasterPage()
         {
             InitializeComponent();
+
+            btnMoonPhase.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.MoonPhase);
+            btnSunrise.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.Sunrise);
+            btnAbout.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.About);
+
+            btnEarth.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.Earth);
+            btnMoon.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.Moon);
+            btnSun.Clicked += (s, e) => PageSelected?.Invoke(this, PageType.Sun);
         }
+
     }
 }
