6accad43b21f1664eac04e08125b95f775af32d8
FlavioCrtt <flavio777crtt@hotmail.com>
2020-01-06 01:13:03 -0300

    Usando conversor de valor

--- Source/binding/FlagFacts/MainPage.xaml.cs
+++ Source/binding/FlagFacts/MainPage.xaml.cs
@@ -32,8 +32,7 @@ namespace FlagFacts
         private void InitializeData()
         {
             country.ItemsSource = (IList) repository.Countries;
-            flagImage.Source = CurrentFlag.GetImageSource();
-
+            
             // Set the binding context
             this.BindingContext = CurrentFlag;
         }
