5662e8ecddd10e1fbaa4e3242288f4cd1035fe81
FlavioCrtt <flavio777crtt@hotmail.com>
2020-01-10 02:43:18 -0300

    Adicionando efeito de sombra no botão

--- Source/effect/ControlExplorer/ControlExplorer.Android/MyButtonGradientEffect.cs
+++ Source/effect/ControlExplorer/ControlExplorer.Android/MyButtonGradientEffect.cs
@@ -1,19 +1,40 @@
-﻿using Android.Graphics.Drawables;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics.Drawables;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using ControlExplorer.Droid;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 
+[assembly: ExportEffect(typeof(MyButtonGradientEffect), "ButtonGradientEffect")]
 namespace ControlExplorer.Droid
 {
-    public class MyButtonGradientEffect : PlatformEffect
+    class MyButtonGradientEffect : PlatformEffect
     {
         Drawable oldDrawable;
 
         protected override void OnAttached()
         {
+            if (Element is Xamarin.Forms.Button == false)
+                return;
+
+            oldDrawable = Control.Background;
+
+            SetGradient();
         }
 
         protected override void OnDetached()
         {
+            if (oldDrawable != null)
+                Control.Background = oldDrawable;
         }
 
         void SetGradient()
