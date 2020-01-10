e4962491427c36b733bf6eadbd715c99b8190bc9
FlavioCrtt <flavio777crtt@hotmail.com>
2020-01-10 02:41:37 -0300

    Projeto para effect

---
+++ Source/effect/ControlExplorer/ControlExplorer.Android/MyButtonGradientEffect.cs
@@ -0,0 +1,31 @@
+﻿using Android.Graphics.Drawables;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+namespace ControlExplorer.Droid
+{
+    public class MyButtonGradientEffect : PlatformEffect
+    {
+        Drawable oldDrawable;
+
+        protected override void OnAttached()
+        {
+        }
+
+        protected override void OnDetached()
+        {
+        }
+
+        void SetGradient()
+        {
+            var xfButton = Element as Xamarin.Forms.Button;
+
+            var colorTop = xfButton.BackgroundColor;
+            var colorBottom = Xamarin.Forms.Color.Black;
+
+            var drawable = Gradient.GetGradientDrawable(colorTop.ToAndroid(), colorBottom.ToAndroid());
+
+            Control.SetBackground(drawable);
+        }
+    }
+}
